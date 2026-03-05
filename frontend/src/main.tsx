d2a58dca1249db14edfa702094bb3e635b8a6b6d
kumarananda <coderanandakumar@gmail.com>
2026-03-05 05:35:16 +0000

    Update frontend assets and configuration
    - Updated script and stylesheet references in index.html and portal.html to new asset versions.
    - Added symlink for node_modules directory.
    - Modified proxyOptions to enable WebSocket support.
    - Enhanced main.tsx to include socket configuration for FrappeProvider.
    - Updated vite.config.ts to set server host to '0.0.0.0'.
    - Added yarn.lock file for dependency management.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
+import { FrappeProvider } from "frappe-react-sdk";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
-import { FrappeProvider } from "frappe-react-sdk";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <FrappeProvider
       siteName="http://localhost:8001"
+      
       enableSocket={true}
       socketPort="9001"
     >
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import proxyOptions from "./proxyOptions";
 
 // https://vitejs.dev/config/
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 8080,
+    host: '0.0.0.0',
     proxy: proxyOptions,
   },
   resolve: {
